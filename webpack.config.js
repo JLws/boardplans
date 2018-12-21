e2f15293156f309d2b4e35ae56e98b7132c52bbd
JLws <romanel65@gmail.com>
2018-12-21 10:27:24 +0200

    add babel, css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
-var path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist')
-	}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				loader: 'babel-loader',
+				exclude: '/node_modules/'
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract(
+				{
+					fallback: 'style-loader',
+					use: ['css-loader']
+				})
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin({filename: 'style.css'})
+	]
 };
